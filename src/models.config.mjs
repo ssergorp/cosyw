5e3dca7e3fcb031d2a9cad5e96885a6e608d5f51
Immanence <120009219+immanencer@users.noreply.github.com>
2024-12-06 17:40:36 +0000

    add new models

--- src/models.config.mjs
+++ src/models.config.mjs
@@ -1,76 +1,84 @@
 const models = [
-    {
-      "model": "meta-llama/llama-3.2-1b-instruct",
-      "rarity": "common"
-    },
-    {
-      "model": "meta-llama/llama-3.2-3b-instruct",
-      "rarity": "common"
-    },
-    {
-      "model": "eva-unit-01/eva-qwen-2.5-72b",
-      "rarity": "rare"
-    },
-    {
-      "model": "openai/gpt-4o",
-      "rarity": "legendary"
-    },
-    {
-      "model": "meta-llama/llama-3.1-405b-instruct",
-      "rarity": "legendary"
-    },
-    {
-      "model": "anthropic/claude-3-opus:beta",
-      "rarity": "legendary"
-    },
-    {
-      "model": "anthropic/claude-3.5-sonnet:beta",
-      "rarity": "legendary"
-    },
-    {
-      "model": "anthropic/claude-3.5-haiku:beta",
-      "rarity": "uncommon"
-    },
-    {
-      "model": "neversleep/llama-3.1-lumimaid-70b",
-      "rarity": "rare"
-    },
-    {
-      "model": "nvidia/llama-3.1-nemotron-70b-instruct",
-      "rarity": "rare"
-    },
-    {
-      "model": "meta-llama/llama-3.1-70b-instruct",
-      "rarity": "uncommon"
-    },
-    {
-      "model": "pygmalionai/mythalion-13b",
-      "rarity": "uncommon"
-    },
-    {
-      "model":"mistralai/mistral-large-2411",
-      "rarity": "uncommon"
-    },
-    {
-      "model": "qwen/qwq-32b-preview",
-      "rarity": "uncommon"
-    },
-    {
-      "model": "eva-unit-01/eva-qwen-2.5-72b",
-      "rarity": "uncommon"
-    },
-    {
-      "model": "gryphe/mythomax-l2-13b",
-      "rarity": "common"
-    },
-    {
-      "model": "google/gemini-flash-1.5-8b",
-      "rarity": "common"
-    },
-    {
-      "model": "x-ai/grok-beta",
-      "rarity": "legedary"
-    }
-  ];
+  {
+    "model": "meta-llama/llama-3.2-1b-instruct",
+    "rarity": "common"
+  },
+  {
+    "model": "meta-llama/llama-3.2-3b-instruct",
+    "rarity": "common"
+  },
+  {
+    "model": "eva-unit-01/eva-qwen-2.5-72b",
+    "rarity": "rare"
+  },
+  {
+    "model": "openai/gpt-4o",
+    "rarity": "legendary"
+  },
+  {
+    "model": "meta-llama/llama-3.1-405b-instruct",
+    "rarity": "legendary"
+  },
+  {
+    "model": "anthropic/claude-3-opus:beta",
+    "rarity": "legendary"
+  },
+  {
+    "model": "anthropic/claude-3.5-sonnet:beta",
+    "rarity": "legendary"
+  },
+  {
+    "model": "anthropic/claude-3.5-haiku:beta",
+    "rarity": "uncommon"
+  },
+  {
+    "model": "neversleep/llama-3.1-lumimaid-70b",
+    "rarity": "rare"
+  },
+  {
+    "model": "nvidia/llama-3.1-nemotron-70b-instruct",
+    "rarity": "rare"
+  },
+  {
+    "model": "meta-llama/llama-3.1-70b-instruct",
+    "rarity": "uncommon"
+  },
+  {
+    "model": "pygmalionai/mythalion-13b",
+    "rarity": "uncommon"
+  },
+  {
+    "model": "mistralai/mistral-large-2411",
+    "rarity": "uncommon"
+  },
+  {
+    "model": "qwen/qwq-32b-preview",
+    "rarity": "uncommon"
+  },
+  {
+    "model": "eva-unit-01/eva-qwen-2.5-72b",
+    "rarity": "uncommon"
+  },
+  {
+    "model": "gryphe/mythomax-l2-13b",
+    "rarity": "common"
+  },
+  {
+    "model": "google/gemini-flash-1.5-8b",
+    "rarity": "common"
+  },
+  {
+    "model": "x-ai/grok-beta",
+    "rarity": "legedary"
+  },
+  {
+    "model": "undi95/remm-slerp-l2-13b",
+    "rarity": "uncommon"
+  },
+  {
+    "model": "meta-llama/llama-3.3-70b-instruct",
+    "rarity": "uncommon"
+  }
+];
 
 export default models;
